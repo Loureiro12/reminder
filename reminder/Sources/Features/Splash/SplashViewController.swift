11ff20d13e388ba14a607e72b549c5a902e73eec
André Loureiro <contato.loureiro1@gmail.com>
2024-12-23 22:28:08 -0300

    Localizable

---
+++ reminder/Sources/Features/Splash/SplashViewController.swift
@@ -0,0 +1,56 @@
+//
+//  SplashViewController.swift
+//  reminder
+//
+//  Created by Andre Loureiro on 21/12/24.
+//
+
+import Foundation
+import UIKit
+
+class SplashViewController: UIViewController {
+    let contentView = SplashView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setup()
+    }
+    
+    private func setup() {
+        self.view.addSubview(contentView)
+        // não vai mostrar a navigation bar nessa tela
+        self.navigationController?.navigationBar.isHidden = true
+        self.view.backgroundColor = Colors.primaryBlueBase
+        setupGesture()
+        
+        setupContrainsts()
+    }
+    
+    private func setupContrainsts() {
+        NSLayoutConstraint.activate([
+            
+            contentView.topAnchor.constraint(equalTo: view.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+        
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func setupGesture() {
+            let tapGesture = UITapGestureRecognizer(target: self, action: #selector(showLoginBottomSheet))
+        self.view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc
+    private func showLoginBottomSheet() {
+        let loginBottomSheet = LoginBottomSheetViewController()
+        loginBottomSheet.modalPresentationStyle = .overCurrentContext
+        loginBottomSheet.modalTransitionStyle = .crossDissolve
+        self.present(loginBottomSheet, animated: false) {
+            loginBottomSheet.animateShow()
+        }
+    }
+}
